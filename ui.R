6dfd335e529486ce427335515dcabf5cb7ba1c50
adumit <andrew.dumit@gmail.com>
2015-07-22 11:16:27 -0700

    Add CC license to ui.R

--- ui.R
+++ ui.R
@@ -1,3 +1,6 @@
+#Shiny Notes by Andrew Dumit is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.
+#Based on a work at https://github.com/adumit/shiny_Notes.
+
 library(shiny)
 library(shinydashboard)
 source('server.R', local=TRUE)
